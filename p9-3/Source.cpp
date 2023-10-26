261985f6b4e31c9b773177430cabd3b79183d228
Elizabeth Burkholder <e-burkholder@onu.edu>
2023-10-26 10:40:43 -0400

    completed displayRun.

--- p9-3/Source.cpp
+++ p9-3/Source.cpp
@@ -1,1 +1,36 @@
-//
+#include <iostream>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void displayRun(int values[], int size);
+
+int main() {
+	int tosses[20];
+	srand(0);
+
+	for (int i = 0; i < 20; i++) {
+		tosses[i] = rand() % 6 + 1;
+	}
+	displayRun(tosses, 20);
+}
+
+void displayRun(int values[], int size) {
+	for (int i = 0; i < size; i++) {
+		int number = values[i + 1];
+		if (i > 0) {
+			cout << " ";
+		}
+
+		if (values[i] == number && values[i] == values[i + 1] && values[i + 1] != values[i + 2]) {
+			cout << "("<<values[i]<<" "<<values[i + 1] << ")";
+			i++;
+			continue;
+		}
+		
+		cout << values[i];
+		
+	}
+
+}
