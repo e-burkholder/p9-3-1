4d26befa9fcc68929eeef43046c03eafab2b3ee4
Elizabeth Burkholder <e-burkholder@onu.edu>
2023-10-26 10:45:46 -0400

    completed hasRun

--- p9-3/Source.cpp
+++ p9-3/Source.cpp
@@ -5,15 +5,26 @@
 using namespace std;
 
 void displayRun(int values[], int size);
+bool hasRun(int values[], int size);
 
 int main() {
 	int tosses[20];
+	bool isRun;
 	srand(0);
 
 	for (int i = 0; i < 20; i++) {
 		tosses[i] = rand() % 6 + 1;
 	}
+
+	isRun = hasRun(tosses, 20);
+	if (isRun == true) {
+		cout << "has run: ";
+	}
+	else {
+		cout << "has no run: ";
+	}
 	displayRun(tosses, 20);
+
 }
 
 void displayRun(int values[], int size) {
@@ -33,4 +44,13 @@ void displayRun(int values[], int size) {
 		
 	}
 
+}
+
+bool hasRun(int values[], int size) {
+	for (int i = 0; i < size; i++) {
+		int number = values[i + 1];
+		if (values[i] == number && values[i] == values[i + 1] && values[i + 1] != values[i + 2]) {
+			return true;
+		}
+	}
 }
